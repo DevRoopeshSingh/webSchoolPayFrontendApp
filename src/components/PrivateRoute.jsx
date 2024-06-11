1b3c9553f6ea5553b190989c16f833389c781604
roopesh User 2 <roopeshsingh993@gmail.com>
2024-06-11 15:38:47 +0530

    added private routes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import { Route, Navigate } from 'react-router-dom';
-
-// const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
-//   return isAuthenticated ? (
-//     <Route {...rest} element={element} />
-//   ) : (
-//     <Navigate to="/login" replace />
-//   );
-// };
-
-// export default PrivateRoute;
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
